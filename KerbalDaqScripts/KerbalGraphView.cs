65c38fa20066f8092fcd61c50dafb0f0a0f4eea6
Pavel Krupets <usaguerrilla@hotmail.com>
2018-06-08 14:18:44 -0500

    - added graph view camera rendering (renders game object instead of a graph for now)

--- KerbalDaqScripts/KerbalGraphView.cs
+++ KerbalDaqScripts/KerbalGraphView.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace KerbalDaqScripts
 {
     public class KerbalGraphView : MonoBehaviour
     {
+        public Camera KerbalGraphViewCamera;
+        public RawImage KerbalGraphRawImage;
+        public RenderTexture KerbalGraphRenderTexture;
         public KerbalScienceStation KerbalScienceStation;
+
+        void Update()
+        {
+            KerbalGraphViewCamera.Render();
+        }
 
         void Start()
         {
             Debug.Log("KPL KerbalGraphView::Start");
+            KerbalGraphViewCamera.transform.SetParent(Camera.main.transform, false);
         }
 
         void OnDisable()
